63f7d7a60ba0aaf7e8c51cf61b539184f5eaad02
furkanturedi <furkantrd52@gmail.com>
2025-10-26 19:54:19 +0300

    düzeltildi

--- minitalk.h
+++ minitalk.h
@@ -1,6 +1,11 @@
 #ifndef MINITALK_H
 # define MINITALK_H
 
+# ifndef _POSIX_C_SOURCE
+#  define _POSIX_C_SOURCE 200809L
+# endif
+
+# include <sys/types.h>
 # include <signal.h>
 # include <unistd.h>
 # include <stdlib.h>
